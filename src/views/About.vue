9c21958cb84283c180d6ea35203f53dcc3f35bbd
our ame <you@example.com>
2019-10-25 19:27:43 +0700

    Добавил резюме

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,61 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row">
+    <div class="col s12">
+      <h3>Кто я?</h3>
+      <p>Я front-end разработчик, который использует Vuejs в работе. <br>
+      Для прототипрования использую bootstrap 4 или https://materializecss.com/. <br>
+      При работе использую scss, а так же современные возможности css (css var, ...). <br>
+      Работаю с css flex. <br>
+      Работаю с css grid, но т.к. у него не полная поддержка браузерами, использую в частных случаях.
+      </p>
+      <h3>С чем я работаю?</h3>
+      <p>
+      Имею опыт работы с php, в частности с фреймворком https://codeigniter.com (3 и 4 (сейчас rс) версиями), <br>
+      БД MySQL или SQLite.
+      </p>
+      <h3>Что я делаю?</h3>
+      <p>
+      Основное направление разработка web приложений, соответственно, использование codeingnter как json api сервер.
+      </p>
+      <h3>Где можно еще найти информацию?</h3>
+      <p>
+      Мой профиль на stack overflow: https://ru.stackoverflow.com/users/208471/Николай
+      </p>
+      <p>
+      Мои репозитории на github
+      https://github.com/idushii
+      </p>
+      <h3>Кстати</h3>
+      <p>
+      Знаком с ssr (https://ru.nuxtjs.org/), работал с node js (azura).
+      Реализовывал программы c# (WPF) (простые).
+      Реализовывал простые приложения на delphi.
+      </p>
+      <h3>Что я умею?</h3>
+      <ol>
+        <li>Web приложения с использванием VueJS <i>например калькулятор</i> </li>
+        <li>Web сайты с использованием VueJS<i>, а так же wordpress</i> </li>
+        <li>Настольные приложения с использованием ElectronJS и Vue JS </li>
+        <li>Мобильные приложения с использованием NativeScript и Vue JS <i>пока что учусь</i> </li>
+      </ol>
+      <h3>Образование</h3>
+      <p>
+      Закончил бакалавриат ВКИ НГУ по направлению программирование.
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  font-size: 1.3rem;
+}
+</style>
+
