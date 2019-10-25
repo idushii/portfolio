286a94db323141e1d4d974c31d3436a51fc74c10
our ame <you@example.com>
2019-10-25 18:38:54 +0700

    Отобразил список проектов на главной странице

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <list-item-record 
+      class="col s6 m4" 
+      v-for="(item, index) in items" :key="`item-${index}`" 
+      :title="item.title"
+      :desc="item.desc"
+      :link="item.link"
+      :links="item.links"
+      :prev="item.prev"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ListItemRecord from "../components/list-item-record";
 export default {
-  name: 'home',
+  name: "home",
+  computed: {
+    items() {
+      return this.$store.state.projects
+    }
+  },
   components: {
-    HelloWorld
+    ListItemRecord
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.row {
+  margin-top: 3rem;
+}
+</style>
