5846cbf9d70d0e85837c9f82ae6012a55fbe6955
our ame <you@example.com>
2019-11-15 00:04:26 +0700

    Полменял порядок ссылок

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
       <div class="nav-wrapper">
         <a href="#" class="brand-logo">ИП Ильинов Николай</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><router-link to="/about">Резюме</router-link></li>
           <li><router-link to="/">Портфолио</router-link></li>
+          <li><router-link to="/about">Резюме</router-link></li>
         </ul>
       </div>
     </nav>
