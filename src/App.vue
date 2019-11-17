128f27fa828216cd44e1d0b6b74ba0b8df6fbae3
our ame <you@example.com>
2019-11-17 14:15:53 +0700

    Подключл ts

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,12 @@
       <div class="nav-wrapper indigo darken-1">
         <a href="#" class="brand-logo">ИП Ильинов Николай</a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><router-link to="/">Портфолио</router-link></li>
-          <li><router-link to="/about">Резюме</router-link></li>
+          <li>
+            <router-link to="/">Портфолио</router-link>
+          </li>
+          <li>
+            <router-link to="/about">Резюме</router-link>
+          </li>
         </ul>
       </div>
     </nav>
@@ -14,6 +18,15 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 .brand-logo {
