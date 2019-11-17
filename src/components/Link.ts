d156b94dbb3e8778424877ca37844e118f05e8ec
our ame <you@example.com>
2019-11-17 15:55:56 +0700

    Добавил класс Link

--- src/components/Link.ts
+++ src/components/Link.ts
@@ -1,6 +1,26 @@
 interface ILink {
     link: string
     title: string
+
+    url: string
 }
 
-export {ILink}
+class Link implements ILink {
+    title: string = ''
+    link: string = ''
+
+    constructor(title: string, link?: string) {
+        if (link) this.title = title
+        this.link = link || title
+    }
+
+    get url() {
+        return this.link
+    }
+
+    set url(link)  {
+        this.link = link
+    }
+}
+
+export { ILink, Link }
