3d3014573de13c0e55920b434bdf779d70b55673
our ame <you@example.com>
2019-12-05 11:14:07 +0700

    Добавил страницу контакты

---
+++ src/views/Contacts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col s12">
+      <h3>Контакты</h3>
+      <p>Email: idushii-mail@yandex.ru</p>
+      <p>Skype: nicolay1232</p>
+      <p>Telegram: @ilinov</p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  font-size: 1.3rem;
+}
+</style>
+
